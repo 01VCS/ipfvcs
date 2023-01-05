4d3fa6f14c8c3d9693c0407470ccce483801dfa0
Daniella Mesquita <mesquitadaniella@proton.me>
2023-01-05 18:29:33 -0300

    Prepare support for compiling into C/Rust

---
+++ compile.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#shc -f ipfvcs
+
+#sudo apt install build-essential llvm-6.0 clang-6.0 libclang-6.0-dev cmake libssl-dev pkg-config python3
+#cargo install c2rust
+
+#cargo build --release
